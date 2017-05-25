4738990196533609e63a0ae6216848a7f68f30bb
Trong-Nghia Nguyen <nghia@viisix.space>
2017-05-25 23:11:06 +0700

    [AlgoriJS] Update font, main containers' width

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,8 +14,48 @@ FILE_NAME=basscss.min.css
 OLD_FILE=${TEMPLATE_STATIC_DIR}/css/${FILE_NAME}
 NEW_FILE=${TEMPLATE_STATIC_DIR}/css/n.${FILE_NAME}
 curl -Ls https://unpkg.com/basscss/css/basscss.min.css > ${NEW_FILE}
-OLD_FILE_CHECKSUM=`md5 ${OLD_FILE} | awk '{ print $4 }'`
-NEW_FILE_CHECKSUM=`md5 ${NEW_FILE} | awk '{ print $4 }' `
+if [ -f ${OLD_FILE} ]; then
+    OLD_FILE_CHECKSUM=`md5 ${OLD_FILE} | awk '{ print $4 }'`
+else
+    OLD_FILE_CHECKSUM=''
+fi
+NEW_FILE_CHECKSUM=`md5 ${NEW_FILE} | awk '{ print $4 }'`
+if [ "$OLD_FILE_CHECKSUM" != "$NEW_FILE_CHECKSUM" ]; then
+    echo "Updating ${FILE_NAME}"
+    mv ${NEW_FILE} ${OLD_FILE}
+else
+    rm ${NEW_FILE}
+fi
+
+# vue.js
+FILE_NAME=vue.js
+OLD_FILE=${TEMPLATE_STATIC_DIR}/js/${FILE_NAME}
+NEW_FILE=${TEMPLATE_STATIC_DIR}/js/n.${FILE_NAME}
+curl -Ls https://vuejs.org/js/vue.js > ${NEW_FILE}
+if [ -f ${OLD_FILE} ]; then
+    OLD_FILE_CHECKSUM=`md5 ${OLD_FILE} | awk '{ print $4 }'`
+else
+    OLD_FILE_CHECKSUM=''
+fi
+NEW_FILE_CHECKSUM=`md5 ${NEW_FILE} | awk '{ print $4 }'`
+if [ "$OLD_FILE_CHECKSUM" != "$NEW_FILE_CHECKSUM" ]; then
+    echo "Updating ${FILE_NAME}"
+    mv ${NEW_FILE} ${OLD_FILE}
+else
+    rm ${NEW_FILE}
+fi
+
+# vue.min.js
+FILE_NAME=vue.min.js
+OLD_FILE=${TEMPLATE_STATIC_DIR}/js/${FILE_NAME}
+NEW_FILE=${TEMPLATE_STATIC_DIR}/js/n.${FILE_NAME}
+curl -Ls https://vuejs.org/js/vue.min.js > ${NEW_FILE}
+if [ -f ${OLD_FILE} ]; then
+    OLD_FILE_CHECKSUM=`md5 ${OLD_FILE} | awk '{ print $4 }'`
+else
+    OLD_FILE_CHECKSUM=''
+fi
+NEW_FILE_CHECKSUM=`md5 ${NEW_FILE} | awk '{ print $4 }'`
 if [ "$OLD_FILE_CHECKSUM" != "$NEW_FILE_CHECKSUM" ]; then
     echo "Updating ${FILE_NAME}"
     mv ${NEW_FILE} ${OLD_FILE}
@@ -52,8 +92,6 @@ if [ "$UPGRADING" == "1" ]; then
     curl -Ls https://jmblog.github.io/color-themes-for-google-code-prettify/themes/atelier-dune-light.min.css \
         > atelier-dune-light.min.css
 fi
-
-
 
 # Clean up
 cd ${BASEDIR} && rm -rf ${TEMP_DIR}
